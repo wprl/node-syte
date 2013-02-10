67929a7eb1e8eb8a50b1316c43e3fffaec2477d9
William P. Riley-Land <william.riley.land@gmail.com>
2013-02-10 17:12:45 -0600

    Twitter OAuth working

--- services/twitter.js
+++ services/twitter.js
@@ -7,24 +7,21 @@ var app = module.exports = express();
 app.get('/:username', function (request, response, next) {
   var username = request.params.username;
   var twitterRequest = {
-    url: config.services.twitter.api.url + '/statuses/user_timeline.json',
+    json: true,
+    url: config.services.twitter.api.url + '1/statuses/user_timeline.json',
     qs: {
       include_rts: false,
       exclude_replies: true,
       count: 50,
       screen_name: username
+    },
+    oauth: {
+      consumer_key: config.services.twitter.consumer.key,
+      consumer_secret: config.services.twitter.consumer.secret,
+      token: config.services.twitter.user.key,
+      token_secret: config.services.twitter.user.secret
     }
   };
-
-  // TODO
-  // twitter = OAuth1Service(
-  //   name='twitter',
-  //   consumer_key=settings.TWITTER_CONSUMER_KEY,
-  //   consumer_secret=settings.TWITTER_CONSUMER_SECRET,
-  //   request_token_url=settings.TWITTER_API_URL + 'oauth/request_token',
-  //   access_token_url=settings.TWITTER_API_URL + 'oauth/access_token',
-  //   authorize_url=settings.TWITTER_API_URL + 'oauth/authorize',
-  //   header_auth=True)
 
   r.get(twitterRequest).pipe(response);
 });
