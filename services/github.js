cb39664a3a4c1c24b95c62998f1a397b464ffb49
William P. Riley-Land <william.riley.land@gmail.com>
2013-02-11 18:54:30 -0600

    A l'il bit of progress on GitHub OAuth

--- services/github.js
+++ services/github.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var config = require('config');
 var r = require('request');
+var _ = require('underscore');
 
 var app = module.exports = express();
+
+app.get('/auth', function auth (request, response, next) {
+  if (!config.services.github.oauth.enabled) return response.send(404);
+
+  var code = request.query.code;
+  var errorDescription = request.query.error_description;
+  var oauthRequest = {
+    url: config.services.github.oauth.accessTokenUrl,
+    qs: {
+      client_id: config.services.github.oauth.client.id,
+      client_secret: config.services.github.oauth.client.secret,
+      redirect_uri: config.site.rootUrl + 'github/auth/',
+      code: code
+    }
+  };
+  var redirectUrl = config.services.github.oauth.authorizeUrl + '?client_id='
+    + config.services.github.oauth.client.id + '&redirect_uri='
+    + config.site.rootUrl + 'github/auth/&response_type=code';
+
+  if (!code && !errorDescription) return response.redirect(redirectUrl);
+
+  r.post(oauthRequest, function (error, postResponse, body) {
+    if (error) return next(new Error(error));
+
+    var context = {
+      token: body.access_token,
+      error: body.error
+    };
+
+    _.extend(context, config);
+
+    response.render('github_auth.html', context);
+  });
+});
 
 app.get('/:username', function (request, response, next) {
   var username = request.params.username;
@@ -13,63 +48,24 @@ app.get('/:username', function (request, response, next) {
     }
   };
   var reposRequest = {
-    url: config.services.github.api.url + 'users/' + username + '/repos'
+    url: config.services.github.api.url + 'users/' + username + '/repos',
     qs: {
       access_token: config.services.github.accessToken
     }
   };
 
-  r.get(userRequest, function (error, response, user) {
+  r.get(userRequest, function (error, getResponse, user) {
     if (error) return next(new Error(error));
 
-    r.get(reposRequest, function (error, response, repos) {
+    r.get(reposRequest, function (error, getResponse, repos) {
       if (error) return next(new Error(error));
 
       var content = {
         user: user,
         repos: repos.sort( function (a, b) { a.updated_at - b.updated_at })
-      }
+      };
 
       response.json(content);
-    }
-  }
+    });
+  });
 });
-
-// TODO
-// #Github Integration
-// if settings.GITHUB_OAUTH_ENABLED:
-//     urlpatterns += patterns('',
-//         url(r'^github/auth/?$', 'syte.views.github.github_auth'),
-//     )
-// routes.auth = function auth (request, response, next) {
-//   context = dict()
-//   code = request.GET.get('code', None)
-//   error = request.GET.get('error_description', None)
-
-//   if not code and not error:
-//     return redirect('{0}?client_id={1}&redirect_uri={2}github/auth/&response_type=code'.format(
-//       settings.GITHUB_OAUTH_AUTHORIZE_URL,
-//       settings.GITHUB_CLIENT_ID,
-//       settings.SITE_ROOT_URI))
-
-//   if code:
-//     r = requests.post(settings.GITHUB_OAUTH_ACCESS_TOKEN_URL, data={
-//       'client_id': settings.GITHUB_CLIENT_ID,
-//       'client_secret': settings.GITHUB_CLIENT_SECRET,
-//       'redirect_uri': '{0}github/auth/'.format(settings.SITE_ROOT_URI),
-//       'code': code,
-//     }, headers={'Accept': 'application/json'})
-
-//   try:
-//   data = r.json
-//   error = data.get('error', None)
-//   except:
-//   error = r.text
-
-//   if not error:
-//     context['token'] = data['access_token']
-
-//   if error:
-//     context['error'] = error
-
-//   return render(request, 'github_auth.html', context)
